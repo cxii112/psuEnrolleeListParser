105666e48f92e549d18fce06acbe2e825cdc56b5
@cxii112 <lyakhovoy.d@ya.ru>
2021-08-10 16:40:22 +0500

    Изменение структуры класса. Теперь хранит словарь <код направления>-<данные о согласиях>

--- src/main/java/Enrollee.java
+++ src/main/java/Enrollee.java
@@ -1,15 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Enrollee
 {
-    public final int index;
     public final long id;
-    public final boolean hasAgreement;
     public final int pointsSum;
 
-    public Enrollee(int index, long id, boolean hasAgreement, int pointsSum)
+    private Map<String, EnrolleeSpecData> specDataMap = new HashMap<>();
+
+    public Enrollee(long id, int pointsSum)
     {
-        this.index = index;
         this.id = id;
-        this.hasAgreement = hasAgreement;
         this.pointsSum = pointsSum;
+    }
+
+    public void addSpec(String specKey, EnrolleeSpecData data)
+    {
+        specDataMap.put(specKey, data);
+    }
+
+    public void addSpec(String specKey, int index, boolean hasAgreement)
+    {
+        var data = new EnrolleeSpecData(index, hasAgreement);
+        specDataMap.put(specKey, data);
+    }
+
+    public boolean hasSentTo(String spec)
+    {
+        return specDataMap.containsKey(spec);
+    }
+
+    public String hasAgreementTo()
+    {
+        var keys = specDataMap.keySet();
+        for (String key : keys)
+        {
+            if (specDataMap.get(key).hasAgreement)
+            {
+                return key;
+            }
+        }
+        return null;
+    }
+}
+
+class EnrolleeSpecData
+{
+    public final int index;
+    public final boolean hasAgreement;
+
+    public EnrolleeSpecData(int index, boolean hasAgreement)
+    {
+        this.index = 0;
+        this.hasAgreement = hasAgreement;
     }
 }
