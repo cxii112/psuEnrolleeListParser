ee063da254a44808be9fefabe6e8cd178ceef976
@cxii112 <lyakhovoy.d@ya.ru>
2021-08-10 14:50:52 +0500

    Удаление метода generateTables

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,8 +5,6 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Solution
 {
@@ -20,33 +18,20 @@ public class Solution
         final String SPEC_CODE = args[3] + 'b';
         Document DOM = Jsoup.connect(LIST_URL).get();
         Elements articles = DOM.getElementsByTag("article");
-        articles.removeIf(article -> {
-            Element header = article.getElementsByTag("h2").first();
-            if (header == null) return false;
-            if (header.getElementsByTag("span").isEmpty()) return false;
-            if (header.getElementsByTag("span").first() == null) return false;
-            String specName = header.getElementsByTag("span").first().ownText();
-            if (specName == null || specName.equals("")) return false;
-            if (specName.contains("заочная")) return false;
-            return specName.contains("бакалавриат");
-        });
+        articles.removeIf(Solution::checkIfInternalStudyBachelor);
         System.out.printf("Bachelor articles: %d\n", articles.size());
-        var tables = generateTables(articles);
     }
 
-    private static Map<String, ArrayList<Enrollee>> generateTables(Elements articles)
+    private static boolean checkIfInternalStudyBachelor(Element article)
     {
-        Map<String, ArrayList<Enrollee>> result = new HashMap();
-        articles.forEach(article -> {
-            String specCode = article.getElementsByTag("a")
-                    .first().attr("name");
-            var tables = article.getElementsByTag("table");
-            var paymentFreeTable = generateList(tables.first());
-            var paymentTable = generateList(tables.last());
-            result.put(specCode + 'b', paymentFreeTable);
-            result.put(specCode + 'p', paymentTable);
-        });
-        return result;
+        Element header = article.getElementsByTag("h2").first();
+        if (header == null) return false;
+        if (header.getElementsByTag("span").isEmpty()) return false;
+        if (header.getElementsByTag("span").first() == null) return false;
+        String specName = header.getElementsByTag("span").first().ownText();
+        if (specName == null || specName.equals("")) return false;
+        if (specName.contains("заочная")) return false;
+        return specName.contains("бакалавриат");
     }
 
     private static ArrayList<Enrollee> generateList(Element table)
